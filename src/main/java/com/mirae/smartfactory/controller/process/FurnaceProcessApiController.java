f175ef5916e55c12afe2305c0f6ac9c1f1e1c33e
kim <zabcd121@naver.com>
2022-08-17 22:36:35 +0900

    용해 일지 저장 및 조회 REST API 개발

---
+++ src/main/java/com/mirae/smartfactory/controller/process/FurnaceProcessApiController.java
@@ -0,0 +1,46 @@
+package com.mirae.smartfactory.controller.process;
+
+import com.mirae.smartfactory.dto.process.furnace.FurnaceProcessDto;
+import com.mirae.smartfactory.dto.process.furnace.FurnaceProcessListDto;
+import com.mirae.smartfactory.repository.FurnaceProcessRepository;
+import com.mirae.smartfactory.service.FurnaceProcessService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.time.LocalDate;
+
+@Slf4j
+@RestController
+@RequestMapping("/mirae")
+@RequiredArgsConstructor
+public class FurnaceProcessApiController {
+
+    //private final ProcessService processService;
+    private final FurnaceProcessRepository furnaceProcessRepository;
+    private final FurnaceProcessService furnaceProcessService;
+
+    @GetMapping("/furnaceprocesses")
+    public FurnaceProcessListDto furnaceProcessList(
+            @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd")  LocalDate date
+    ) {
+        return furnaceProcessService.findFurnaceProcesses(date);
+    }
+
+    @ResponseStatus(HttpStatus.MOVED_PERMANENTLY)
+    @PostMapping("/furnaceprocess")
+    public HttpHeaders furnaceProcessSave(@ModelAttribute FurnaceProcessDto furnaceProcessDto) throws URISyntaxException {
+        furnaceProcessService.saveFurnaceProcess(furnaceProcessDto);
+
+        URI redirectUri = new URI("http://localhost:8080/mirae/furnaceprocesses?date=");
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setLocation(redirectUri);
+        return httpHeaders;
+    }
+
+}
