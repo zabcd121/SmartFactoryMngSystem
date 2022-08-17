f6eefa0577e8424035bd402b7aa463538c8ebc3c
kim <zabcd121@naver.com>
2022-08-17 22:37:55 +0900

    용해 일지 저장 및 조회 메서드 추가

---
+++ src/main/java/com/mirae/smartfactory/service/FurnaceProcessService.java
@@ -0,0 +1,48 @@
+package com.mirae.smartfactory.service;
+
+import com.mirae.smartfactory.domain.process.Process;
+import com.mirae.smartfactory.domain.process.furnace.FurnaceProcess;
+import com.mirae.smartfactory.domain.process.furnace.IngredientAnalysis;
+import com.mirae.smartfactory.dto.process.furnace.FurnaceProcessDto;
+import com.mirae.smartfactory.dto.process.furnace.FurnaceProcessListDto;
+import com.mirae.smartfactory.repository.FurnaceProcessRepository;
+import com.mirae.smartfactory.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class FurnaceProcessService {
+
+    private final FurnaceProcessRepository furnaceProcessRepository;
+    private final MemberRepository memberRepository;
+
+    public void saveFurnaceProcess(FurnaceProcessDto furnaceProcessDto) {
+        Process process = Process.createProcessWithDto(furnaceProcessDto.getProcess(), memberRepository.findById(furnaceProcessDto.getProcess().getMemberId()));
+        IngredientAnalysis ingredientAnalysis = IngredientAnalysis.createIngredientAnalysisWithDto(furnaceProcessDto.getIngredientAnalysis());
+
+        FurnaceProcess furnaceProcess = FurnaceProcess.createFurnaceProcessWithDto(furnaceProcessDto, process, ingredientAnalysis);
+
+        furnaceProcessRepository.save(furnaceProcess);
+    }
+
+    public FurnaceProcessListDto findFurnaceProcesses(LocalDate date) {
+        List<FurnaceProcess> furnaceProcesses = furnaceProcessRepository.findListByDate(date);
+
+        List<FurnaceProcessDto> furnaceProcessDtos = furnaceProcesses.stream()
+                .map(fp -> new FurnaceProcessDto(fp))
+                .collect(Collectors.toList());
+
+        FurnaceProcessListDto result = new FurnaceProcessListDto(furnaceProcessDtos);
+
+        return result;
+    }
+
+
+}
