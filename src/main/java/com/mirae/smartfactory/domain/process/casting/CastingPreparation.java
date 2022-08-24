ae0b9484aab7042a3ef457314062b6d1c2cab469
kim <zabcd121@naver.com>
2022-08-25 02:52:41 +0900

    주조공정 이력카드에서 1.주조준비에 해당하는 Entity 생성

--- src/main/java/com/mirae/smartfactory/domain/process/casting/CastingPreparation.java
+++ src/main/java/com/mirae/smartfactory/domain/process/casting/CastingPreparation.java
@@ -1,6 +1,10 @@
-package com.mirae.smartfactory.domain.casting;
+package com.mirae.smartfactory.domain.process.casting;
 
+import com.mirae.smartfactory.dto.process.casting.CastingDto;
+import com.mirae.smartfactory.dto.process.casting.CastingPreparationDto;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
@@ -8,21 +12,39 @@ import static javax.persistence.FetchType.*;
 
 @Entity
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class CastingPreparation {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long castingPreparationId;
 
-    @OneToOne(fetch = LAZY)
-    @JoinColumn(name = "castingId")
-    private Casting casting;
-
-    private long rotorSpeed1;
-    private long rotorSpeed2;
-    private long arAmount1;
-    private long arAmount2;
-    private int operatingDuration;
-    private long outGassingTemperature;
-    private long ATBSpeed;
+    private Integer rotorSpeed1;
+    private Integer rotorSpeed2;
+    private Integer arAmount1;
+    private Integer arAmount2;
+    private Integer operatingDuration;
+    private Integer outGassingTemperature;
+    private Integer ATBSpeed;
     private String castingPreparationMore;
+
+    public CastingPreparation(Integer rotorSpeed1, Integer rotorSpeed2, Integer arAmount1, Integer arAmount2, Integer operatingDuration, Integer outGassingTemperature, Integer ATBSpeed, String castingPreparationMore) {
+        this.rotorSpeed1 = rotorSpeed1;
+        this.rotorSpeed2 = rotorSpeed2;
+        this.arAmount1 = arAmount1;
+        this.arAmount2 = arAmount2;
+        this.operatingDuration = operatingDuration;
+        this.outGassingTemperature = outGassingTemperature;
+        this.ATBSpeed = ATBSpeed;
+        this.castingPreparationMore = castingPreparationMore;
+    }
+
+    public static CastingPreparation createCastingPreparationWithDto(CastingPreparationDto cpd) {
+        return CastingPreparation.createCastingPreparation(cpd.getRotorSpeed1(), cpd.getRotorSpeed2(), cpd.getArAmount1(), cpd.getArAmount2(),
+                cpd.getOperatingDuration(), cpd.getOutGassingTemperature(), cpd.getATBSpeed(), cpd.getCastingPreparationMore());
+    }
+
+    public static CastingPreparation createCastingPreparation(Integer rotorSpeed1, Integer rotorSpeed2, Integer arAmount1, Integer arAmount2, Integer operatingDuration, Integer outGassingTemperature, Integer ATBSpeed, String castingPreparationMore) {
+        return new CastingPreparation(rotorSpeed1, rotorSpeed2, arAmount1, arAmount2,operatingDuration, outGassingTemperature, ATBSpeed, castingPreparationMore);
+    }
 }
