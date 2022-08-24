ebeb4d22a52f3010cd655856be6066b1beb3851d
kim <zabcd121@naver.com>
2022-08-25 03:15:04 +0900

    기본적인 member save, findById 임시 구현

---
+++ src/main/java/com/mirae/smartfactory/repository/MemberRepository.java
@@ -0,0 +1,22 @@
+package com.mirae.smartfactory.repository;
+
+import com.mirae.smartfactory.domain.resource.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+
+@Repository
+@RequiredArgsConstructor
+public class MemberRepository {
+
+    private final EntityManager em;
+
+    public void save(Member member) {
+        em.persist(member);
+    }
+
+    public Member findById(Long id) {
+        return em.find(Member.class, id);
+    }
+}
