d05ed677af7cf01b66dabeb5a9390f16878439b4
kim <zabcd121@naver.com>
2022-08-25 02:50:54 +0900

    최종 결과물인 billet Entity 생성

--- src/main/java/com/mirae/smartfactory/domain/billet/Billet.java
+++ src/main/java/com/mirae/smartfactory/domain/billet/Billet.java
@@ -1,7 +1,10 @@
 package com.mirae.smartfactory.domain.billet;
 
-import com.mirae.smartfactory.domain.furnace.Process;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.mirae.smartfactory.domain.process.Process;
+import com.mirae.smartfactory.dto.BilletDto;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
@@ -9,16 +12,29 @@ import static javax.persistence.FetchType.*;
 
 @Entity
 @Getter
+@NoArgsConstructor
 public class Billet {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long billetId;
 
-    @OneToOne(mappedBy = "billet", fetch = LAZY)
-    private Process process;
+    private Long length;
+    private Integer quantity;
+    private Integer errorQuantity;
+    private String errorReason;
 
-    private long length;
-    private int quantity;
-    private int errorQuantity;
-    private String billetMore;
+    private Billet(Long length, Integer quantity, Integer errorQuantity, String errorReason) {
+        this.length = length;
+        this.quantity = quantity;
+        this.errorQuantity = errorQuantity;
+        this.errorReason = errorReason;
+    }
+
+    public static Billet createBilletWithDto(BilletDto billet) {
+        return Billet.createBillet(billet.getLength(), billet.getQuantity(), billet.getErrorQuantity(), billet.getErrorReason());
+    }
+
+    public static Billet createBillet(Long length, Integer quantity, Integer errorQuantity, String errorReason) {
+        return new Billet(length, quantity, errorQuantity, errorReason);
+    }
 }
