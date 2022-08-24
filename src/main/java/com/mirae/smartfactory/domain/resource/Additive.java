c8afdf83d5243fa4f1cc7626971460e71edee640
kim <zabcd121@naver.com>
2022-08-25 02:59:50 +0900

    si 첨가물을 나타내는 Entity 생성

--- src/main/java/com/mirae/smartfactory/domain/resource/Additive.java
+++ src/main/java/com/mirae/smartfactory/domain/resource/Additive.java
@@ -1,7 +1,11 @@
 package com.mirae.smartfactory.domain.resource;
 
-import com.mirae.smartfactory.domain.furnace.Process;
+import com.mirae.smartfactory.domain.process.Process;
+import com.mirae.smartfactory.dto.AdditiveDto;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 
@@ -9,15 +13,42 @@ import static javax.persistence.FetchType.*;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Additive {
+
+    @Enumerated(value = EnumType.STRING)
+    private final ResourceType resourceType = ResourceType.SI;
+
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long additiveId;
 
+    @Setter
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "processId")
     private Process process;
 
-    private AdditiveName additiveName;
-    private Long additiveWeight;
+    private String additiveName;
+    private long additiveWeight;
+
+    private Additive(String additiveName, long additiveWeight) {
+        this.additiveName = additiveName;
+        this.additiveWeight = additiveWeight;
+    }
+
+//    public void setProcess(Process process) {
+//        this.process = process;
+//        process.getAdditives().add(this);
+//    }
+
+    //== 생성 메서드 ==/
+    public static Additive createAdditiveWithDto(AdditiveDto additiveDto) {
+        return Additive.createAdditive(additiveDto.getAdditiveName(), additiveDto.getAdditiveWeight());
+    }
+
+    public static Additive createAdditive(String additiveName, long additiveWeight) {
+        Additive additive = new Additive(additiveName, additiveWeight);
+
+        return additive;
+    }
 }
