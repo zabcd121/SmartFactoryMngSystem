b582663911c5640bd44821a09745f9e60a0960cf
kim <zabcd121@naver.com>
2022-08-25 02:54:13 +0900

    주조공정 이력카드에서 3.용해로 용탕온도, 4. 냉각수 온도에 해당하는 Entity 생성

--- src/main/java/com/mirae/smartfactory/domain/process/casting/CastingTemperature.java
+++ src/main/java/com/mirae/smartfactory/domain/process/casting/CastingTemperature.java
@@ -1,6 +1,8 @@
-package com.mirae.smartfactory.domain.casting;
+package com.mirae.smartfactory.domain.process.casting;
 
+import com.mirae.smartfactory.dto.process.casting.CastingTemperatureDto;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
@@ -8,24 +10,43 @@ import static javax.persistence.FetchType.LAZY;
 
 @Entity
 @Getter
+@NoArgsConstructor
 public class CastingTemperature {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long castingTemperatureId;
 
-    @OneToOne(fetch = LAZY)
-    @JoinColumn(name = "castingId")
-    private Casting casting;
+    private Integer furnaceTemperatureInit;
+    private Integer furnaceTemperature15M;
+    private Integer furnaceTemperature30M;
+    private Integer furnaceTemperature45M;
+    private Integer furnaceTemperature60M;
+    private Integer furnaceTemperature75M;
+    private Integer furnaceTemperature90M;
+    private Integer coolingWaterTemperatureInit;
+    private Integer coolingWaterTemperatureMiddle;
+    private Integer coolingWaterTemperatureTerminated;
 
-    private long furnaceTemperatureInit;
-    private long furnaceTemperature15M;
-    private long furnaceTemperature30M;
-    private long furnaceTemperature45M;
-    private long furnaceTemperature60M;
-    private long furnaceTemperature75M;
-    private long furnaceTemperature90M;
-    private long coolingWaterTemperatureInit;
-    private long coolingWaterTemperatureMiddle;
-    private long coolingWaterTemperatureTerminated;
+    public CastingTemperature(Integer furnaceTemperatureInit, Integer furnaceTemperature15M, Integer furnaceTemperature30M, Integer furnaceTemperature45M, Integer furnaceTemperature60M, Integer furnaceTemperature75M, Integer furnaceTemperature90M, Integer coolingWaterTemperatureInit, Integer coolingWaterTemperatureMiddle, Integer coolingWaterTemperatureTerminated) {
+        this.furnaceTemperatureInit = furnaceTemperatureInit;
+        this.furnaceTemperature15M = furnaceTemperature15M;
+        this.furnaceTemperature30M = furnaceTemperature30M;
+        this.furnaceTemperature45M = furnaceTemperature45M;
+        this.furnaceTemperature60M = furnaceTemperature60M;
+        this.furnaceTemperature75M = furnaceTemperature75M;
+        this.furnaceTemperature90M = furnaceTemperature90M;
+        this.coolingWaterTemperatureInit = coolingWaterTemperatureInit;
+        this.coolingWaterTemperatureMiddle = coolingWaterTemperatureMiddle;
+        this.coolingWaterTemperatureTerminated = coolingWaterTemperatureTerminated;
+    }
 
+    public static CastingTemperature createCastingTemperatureWithDto(CastingTemperatureDto ctd) {
+        return CastingTemperature.createCastingTemperature(ctd.getFurnaceTemperatureInit(), ctd.getFurnaceTemperature15M(), ctd.getFurnaceTemperature30M(), ctd.getFurnaceTemperature45M(), ctd.getFurnaceTemperature60M(), ctd.getFurnaceTemperature75M(), ctd.getFurnaceTemperature90M(),
+                ctd.getCoolingWaterTemperatureInit(), ctd.getCoolingWaterTemperatureMiddle(), ctd.getCoolingWaterTemperatureTerminated());
+    }
+
+    public static CastingTemperature createCastingTemperature(Integer furnaceTemperatureInit, Integer furnaceTemperature15M, Integer furnaceTemperature30M, Integer furnaceTemperature45M, Integer furnaceTemperature60M, Integer furnaceTemperature75M, Integer furnaceTemperature90M, Integer coolingWaterTemperatureInit, Integer coolingWaterTemperatureMiddle, Integer coolingWaterTemperatureTerminated) {
+        return new CastingTemperature(furnaceTemperatureInit, furnaceTemperature15M, furnaceTemperature30M, furnaceTemperature45M, furnaceTemperature60M, furnaceTemperature75M, furnaceTemperature90M,
+                coolingWaterTemperatureInit, coolingWaterTemperatureMiddle, coolingWaterTemperatureTerminated);
+    }
 }
