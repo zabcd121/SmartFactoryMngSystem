72c5e9af1c93100e4589980e971697311896325f
kim <zabcd121@naver.com>
2022-09-01 21:28:59 +0900

    통계 관련 dto 생성

---
+++ src/main/java/com/mirae/smartfactory/dto/statistics/AshesStatisticsDto.java
@@ -0,0 +1,11 @@
+//package com.mirae.smartfactory.dto.statistics;
+//
+//import com.mirae.smartfactory.domain.statistics.Statistics;
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//
+//@Getter
+//@AllArgsConstructor
+//public class AshesStatisticsDto {
+//    private Statistics ashes;
+//}
---
+++ src/main/java/com/mirae/smartfactory/dto/statistics/StatisticsListDto.java
@@ -0,0 +1,14 @@
+package com.mirae.smartfactory.dto.statistics;
+
+import com.mirae.smartfactory.domain.statistics.Statistics;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class StatisticsListDto {
+    private Statistics ashes;
+
+}
