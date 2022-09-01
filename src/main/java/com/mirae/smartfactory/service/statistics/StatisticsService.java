68bd10127d08d49873e6ffab875cc8b9aa5a66a4
kim <zabcd121@naver.com>
2022-09-01 21:29:47 +0900

    통계의 공통된 service 로직을 처리하는 Service 클래스 생성

---
+++ src/main/java/com/mirae/smartfactory/service/statistics/StatisticsService.java
@@ -0,0 +1,24 @@
+package com.mirae.smartfactory.service.statistics;
+
+import com.mirae.smartfactory.domain.statistics.Statistics;
+import com.mirae.smartfactory.dto.statistics.DailyStatisticsListDto;
+import com.mirae.smartfactory.dto.statistics.StatisticsListDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class StatisticsService {
+
+    private final AshesStatisticsService ashesStatisticsService;
+
+    public DailyStatisticsListDto findTotalDomainDailyStatistics() {
+        Statistics ashes = ashesStatisticsService.findAshesAmountForCurrent7Days();
+
+        StatisticsListDto statisticsListDto = new StatisticsListDto(ashes);
+
+        return new DailyStatisticsListDto(statisticsListDto);
+    }
+}
