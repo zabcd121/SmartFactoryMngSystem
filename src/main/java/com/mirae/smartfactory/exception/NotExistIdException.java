c0dccaf8389438492de02cbf25957b9c40f94bd7
kim <zabcd121@naver.com>
2022-09-04 21:41:09 +0900

    존재하지 않는 id 입력 예외 생성

---
+++ src/main/java/com/mirae/smartfactory/exception/NotExistIdException.java
@@ -0,0 +1,23 @@
+package com.mirae.smartfactory.exception;
+
+public class NotExistIdException extends RuntimeException{
+    public NotExistIdException() {
+        super();
+    }
+
+    public NotExistIdException(String message) {
+        super(message);
+    }
+
+    public NotExistIdException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NotExistIdException(Throwable cause) {
+        super(cause);
+    }
+
+    protected NotExistIdException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
