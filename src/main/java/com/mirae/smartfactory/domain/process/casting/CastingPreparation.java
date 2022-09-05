980e62f8db568b3c5795bf384f56cd80c4e72e1c
kim <zabcd121@naver.com>
2022-09-05 21:01:56 +0900

    ATBSpeed -> atbSpeed로 변경

--- src/main/java/com/mirae/smartfactory/domain/process/casting/CastingPreparation.java
+++ src/main/java/com/mirae/smartfactory/domain/process/casting/CastingPreparation.java
@@ -25,26 +25,26 @@ public class CastingPreparation {
     private Integer arAmount2;
     private Integer operatingDuration;
     private Integer outGassingTemperature;
-    private Integer ATBSpeed;
+    private Integer atbSpeed;
     private String castingPreparationMore;
 
-    public CastingPreparation(Integer rotorSpeed1, Integer rotorSpeed2, Integer arAmount1, Integer arAmount2, Integer operatingDuration, Integer outGassingTemperature, Integer ATBSpeed, String castingPreparationMore) {
+    public CastingPreparation(Integer rotorSpeed1, Integer rotorSpeed2, Integer arAmount1, Integer arAmount2, Integer operatingDuration, Integer outGassingTemperature, Integer atbSpeed, String castingPreparationMore) {
         this.rotorSpeed1 = rotorSpeed1;
         this.rotorSpeed2 = rotorSpeed2;
         this.arAmount1 = arAmount1;
         this.arAmount2 = arAmount2;
         this.operatingDuration = operatingDuration;
         this.outGassingTemperature = outGassingTemperature;
-        this.ATBSpeed = ATBSpeed;
+        this.atbSpeed = atbSpeed;
         this.castingPreparationMore = castingPreparationMore;
     }
 
     public static CastingPreparation createCastingPreparationWithDto(CastingPreparationDto cpd) {
         return CastingPreparation.createCastingPreparation(cpd.getRotorSpeed1(), cpd.getRotorSpeed2(), cpd.getArAmount1(), cpd.getArAmount2(),
-                cpd.getOperatingDuration(), cpd.getOutGassingTemperature(), cpd.getATBSpeed(), cpd.getCastingPreparationMore());
+                cpd.getOperatingDuration(), cpd.getOutGassingTemperature(), cpd.getAtbSpeed(), cpd.getCastingPreparationMore());
     }
 
-    public static CastingPreparation createCastingPreparation(Integer rotorSpeed1, Integer rotorSpeed2, Integer arAmount1, Integer arAmount2, Integer operatingDuration, Integer outGassingTemperature, Integer ATBSpeed, String castingPreparationMore) {
-        return new CastingPreparation(rotorSpeed1, rotorSpeed2, arAmount1, arAmount2,operatingDuration, outGassingTemperature, ATBSpeed, castingPreparationMore);
+    public static CastingPreparation createCastingPreparation(Integer rotorSpeed1, Integer rotorSpeed2, Integer arAmount1, Integer arAmount2, Integer operatingDuration, Integer outGassingTemperature, Integer atbSpeed, String castingPreparationMore) {
+        return new CastingPreparation(rotorSpeed1, rotorSpeed2, arAmount1, arAmount2,operatingDuration, outGassingTemperature, atbSpeed, castingPreparationMore);
     }
 }
