4b96f4286f79d6918349dbec5450364b141bfe49
kim <zabcd121@naver.com>
2022-09-05 21:16:10 +0900

    ResourceNameListDto 타입으로 4가지 리소스들의 nameList 클래스 타입을 통일

--- src/main/java/com/mirae/smartfactory/dto/resource/TotalResourceNameDto.java
+++ src/main/java/com/mirae/smartfactory/dto/resource/TotalResourceNameDto.java
@@ -1,19 +1,15 @@
 package com.mirae.smartfactory.dto.resource;
 
-import com.mirae.smartfactory.dto.resource.BusinessContactNameListDto;
-import com.mirae.smartfactory.dto.resource.IngredientNameListDto;
-import com.mirae.smartfactory.dto.resource.OuterScrapNameListDto;
-import com.mirae.smartfactory.dto.resource.SiNameListDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public class ResourceNameDto {
+public class TotalResourceNameDto {
 
-    private OuterScrapNameListDto outerScrapNameList;
-    private SiNameListDto siNameList;
-    private IngredientNameListDto ingredientNameList;
-    private BusinessContactNameListDto businessContactNameList;
+    private ResourceNameListDto outerScrapNameList;
+    private ResourceNameListDto siNameList;
+    private ResourceNameListDto ingredientNameList;
+    private ResourceNameListDto businessContactNameList;
 
 }
