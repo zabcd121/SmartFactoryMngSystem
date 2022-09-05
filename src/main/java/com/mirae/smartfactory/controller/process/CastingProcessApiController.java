36d9b0861c9aa80fe0148d161d88a7de9f3b7f5c
kim <zabcd121@naver.com>
2022-09-05 20:58:42 +0900

    주조 일지 저장 api HttpServletResponse를 이용하여 Location 설정하도록 코드 수정 및 주조 일지 수정 api 개발

--- src/main/java/com/mirae/smartfactory/controller/process/CastingProcessApiController.java
+++ src/main/java/com/mirae/smartfactory/controller/process/CastingProcessApiController.java
@@ -2,17 +2,17 @@ package com.mirae.smartfactory.controller.process;
 
 import com.mirae.smartfactory.dto.process.casting.CastingDto;
 import com.mirae.smartfactory.dto.process.casting.CastingListDto;
-import com.mirae.smartfactory.dto.process.furnace.FurnaceProcessDto;
+import com.mirae.smartfactory.dto.result.SuccessResult;
 import com.mirae.smartfactory.service.CastingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.net.URI;
-import java.net.URISyntaxException;
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDate;
+
+import static com.mirae.smartfactory.consts.DomainConditionCode.*;
 
 @RestController
 @RequestMapping("/mirae")
@@ -22,22 +22,41 @@ public class CastingProcessApiController {
     private final CastingService castingService;
 
     @GetMapping("/casting")
-    public CastingListDto castingList(
+    public SuccessResult<CastingListDto> castingList(
             @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate date
     ) {
-        return castingService.findCastings(date);
+        CastingListDto castings = castingService.findCastings(date);
+        return new SuccessResult<>(CASTING_SEARCH_SUCCESS, "ok", castings);
     }
 
     @ResponseStatus(HttpStatus.MOVED_PERMANENTLY)
     @PostMapping("/casting")
-    public HttpHeaders furnaceProcessSave(
+    public void castingSave(
             @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate date,
-            @RequestBody CastingDto castingDto) throws URISyntaxException {
+            @RequestBody CastingDto castingDto,
+            HttpServletResponse httpServletResponse){
+
         castingService.saveCasting(castingDto);
 
-        URI redirectUri = new URI("http://localhost:8080/mirae/casting?date=" + date);
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.setLocation(redirectUri);
-        return httpHeaders;
+
+        httpServletResponse.addHeader("Location", "http://localhost:8080/mirae/casting?date=" + date);
+
+//        return new SuccessResult<String>(CASTING_SAVE_SUCCESS, "저장 완료 되었습니다.", id.toString());
+    }
+
+    @ResponseStatus(HttpStatus.MOVED_PERMANENTLY)
+    @PutMapping("/casting/{id}")
+    public void castingUpdate(
+            @PathVariable("id") Long castingId,
+            @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate date,
+            @RequestBody CastingDto castingDto,
+            HttpServletResponse httpServletResponse){
+
+        castingService.updateCasting(castingId, castingDto);
+
+
+        httpServletResponse.addHeader("Location", "http://localhost:8080/mirae/casting?date=" + date);
+
+//        return new SuccessResult<String>(CASTING_SAVE_SUCCESS, "저장 완료 되었습니다.", id.toString());
     }
 }
