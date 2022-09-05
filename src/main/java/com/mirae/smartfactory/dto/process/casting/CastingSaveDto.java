c11a0ec617fda73e88c4890c8f3cae5180ff27e7
kim <zabcd121@naver.com>
2022-09-05 20:11:17 +0900

    사용자의 주조 일지 입력 폼에서 전송된 데이터를 받을 dto 개발

---
+++ src/main/java/com/mirae/smartfactory/dto/process/casting/CastingSaveDto.java
@@ -0,0 +1,43 @@
+package com.mirae.smartfactory.dto.process.casting;
+
+import com.mirae.smartfactory.domain.process.casting.Casting;
+import com.mirae.smartfactory.dto.BilletDto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CastingSaveDto {
+    private Integer dailyProcessId;
+    private LocalDateTime tappingStartTime;
+    private LocalDateTime tappingEndTime;
+    private String operator;
+    private String shifter;
+
+    private String remarks;
+
+    private ProcessSimpleDto process;
+
+    private CastingPreparationDto castingPreparation;
+    private CastingDataDto castingData;
+    private CastingTemperatureDto castingTemperature;
+    private BilletDto billet;
+
+    public CastingSaveDto(Casting casting, Integer dailyProcessId) {
+        this.dailyProcessId = dailyProcessId;
+        this.tappingStartTime = casting.getTappingStartTime();
+        this.tappingEndTime = casting.getTappingEndTime();
+        this.operator = casting.getOperator();
+        this.shifter = casting.getShifter();
+        this.remarks = casting.getRemarks();
+        this.process = new ProcessSimpleDto(casting.getProcess());
+        this.castingPreparation = new CastingPreparationDto(casting.getCastingPreparation());
+        this.castingData = new CastingDataDto(casting.getCastingData());
+        this.castingTemperature = new CastingTemperatureDto(casting.getCastingTemperature());
+        this.billet = new BilletDto(casting.getBillet());
+    }
+}
