a81b8cceb4068c467a08373e7233013caea56eef
kim <zabcd121@naver.com>
2022-09-05 20:55:27 +0900

    일 월 분기 년도 별 평균 재발생량 조회 api 개발

--- src/main/java/com/mirae/smartfactory/controller/StatisticsApiController.java
+++ src/main/java/com/mirae/smartfactory/controller/StatisticsApiController.java
@@ -1,7 +1,10 @@
 package com.mirae.smartfactory.controller;
 
 import com.mirae.smartfactory.domain.statistics.Statistics;
-import com.mirae.smartfactory.service.StatisticsService;
+import com.mirae.smartfactory.dto.statistics.DailyStatisticsListDto;
+import com.mirae.smartfactory.dto.statistics.StatisticsListDto;
+import com.mirae.smartfactory.service.statistics.AshesStatisticsService;
+import com.mirae.smartfactory.service.statistics.StatisticsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,11 +15,54 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class StatisticsApiController {
 
+    private final AshesStatisticsService ashesStatisticsService;
     private final StatisticsService statisticsService;
 
-    @GetMapping("/ashes/daily")
-    public Statistics ashesDayStatistics(){
-        return statisticsService.findAshesAmountForSevenDays();
+    @GetMapping("/daily")
+    public DailyStatisticsListDto dailyStatistics() {
+        return statisticsService.findTotalDomainDailyStatistics();
     }
+
+    @GetMapping("/ashes/daily")
+    public Statistics ashesDailyStatistics(){
+        return ashesStatisticsService.findAshesAmountForCurrent7Days();
+    }
+
+    @GetMapping("/ashes/monthly")
+    public Statistics ashesMonthlyStatistics(){
+        return ashesStatisticsService.findAshesAmountForCurrent12Months();
+    }
+
+    @GetMapping("/ashes/quarterly")
+    public Statistics ashesQuarterlyStatistics(){
+        return ashesStatisticsService.findAshesAmountForCurrent4Quarter();
+    }
+
+    @GetMapping("/ashes/yearly")
+    public Statistics ashesYearlyStatistics(){
+        return ashesStatisticsService.findAshesAmountForCurrent4Years();
+    }
+
+    @GetMapping("/billet/daily")
+    public Statistics billetDailyStatistics(){
+        return ashesStatisticsService.findAshesAmountForCurrent7Days();
+    }
+
+    @GetMapping("/billet/monthly")
+    public Statistics billetMonthlyStatistics(){
+        return ashesStatisticsService.findAshesAmountForCurrent12Months();
+    }
+
+    @GetMapping("/billet/quarterly")
+    public Statistics billetQuarterlyStatistics(){
+        return ashesStatisticsService.findAshesAmountForCurrent4Quarter();
+    }
+
+    @GetMapping("/billet/yearly")
+    public Statistics billetYearlyStatistics(){
+        return ashesStatisticsService.findAshesAmountForCurrent4Years();
+    }
+
+
 
 }
