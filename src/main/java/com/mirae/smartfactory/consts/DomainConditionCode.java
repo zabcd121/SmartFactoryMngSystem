8c2bc3df1e326c25e99b9dbf71734950d15fe320
kim <zabcd121@naver.com>
2022-09-05 20:04:46 +0900

    각 주요 도메인에 대한 상황에 따른 상수 코드 생성

---
+++ src/main/java/com/mirae/smartfactory/consts/DomainConditionCode.java
@@ -0,0 +1,49 @@
+package com.mirae.smartfactory.consts;
+
+public class DomainConditionCode {
+
+    /**
+     * Member : 01
+     */
+    public static final String LOGIN_SUCCESS = "0100";
+    public static final String ID_NULL_CODE = "0101";
+    public static final String PW_NULL_CODE = "0102";
+    public static final String NOT_EXIST_ID_CODE = "0103";
+    public static final String INVALID_PW_CODE = "0104";
+    public static final String LOGOUT_SUCCESS = "0105";
+
+    /**
+     * FurnaceProcess : 02
+     */
+
+    public static final String FURNACEPROCESS_SEARCH_SUCCESS = "0200";
+    public static final String FURNACEPROCESS_SEARCH_FAIL = "0201";
+    public static final String FURNACEPROCESS_SAVE_SUCCESS = "0202";
+    public static final String FURNACEPROCESS_SAVE_FAIL = "0203";
+
+    /**
+     * Casting: 03
+     */
+    public static final String CASTING_SEARCH_SUCCESS = "0300";
+    public static final String CASTING_SEARCH_FAIL = "0301";
+    public static final String CASTING_SAVE_SUCCESS = "0302";
+    public static final String CASTING_SAVE_FAIL = "0303";
+
+
+    /**
+     * Resource : 04
+     */
+    public static final String OUTERSCRAP_ADD_SUCCESS = "0400";
+    public static final String OUTERSCRAP_ADD_FAIL = "0401";
+    public static final String SI_ADD_SUCCESS = "0402";
+    public static final String SI_ADD_FAIL = "0403";
+    public static final String INGREDIENT_ADD_SUCCESS = "0404";
+    public static final String INGREDIENT_ADD_FAIL = "0405";
+    public static final String CONTACT_ADD_SUCCESS = "0406";
+    public static final String CONTACT_ADD_FAIL = "0407";
+
+    public static final String RESOURCE_REMOVE_SUCCESS = "0408";
+    public static final String RESOURCE_TOTAL_SEARCH_SUCCESS = "0409";
+
+
+}
