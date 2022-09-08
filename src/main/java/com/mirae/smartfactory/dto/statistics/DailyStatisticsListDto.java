e146933d285403a55c8ffe78a1bf8a4e49e50171
kim <zabcd121@naver.com>
2022-09-08 14:49:54 +0900

    day변수의 타입 네이밍 변경

--- src/main/java/com/mirae/smartfactory/dto/statistics/DailyStatisticsListDto.java
+++ src/main/java/com/mirae/smartfactory/dto/statistics/DailyStatisticsListDto.java
@@ -1,11 +1,10 @@
 package com.mirae.smartfactory.dto.statistics;
 
-import com.mirae.smartfactory.domain.statistics.Statistics;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class DailyStatisticsListDto {
-    StatisticsListDto day;
+    AllDomainStatisticsListDto day;
 }
