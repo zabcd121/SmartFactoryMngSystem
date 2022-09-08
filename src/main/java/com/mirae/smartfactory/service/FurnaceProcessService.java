9d36e9b8de31c7004556f2b20c2b4bab2916d869
kim <zabcd121@naver.com>
2022-09-08 14:20:11 +0900

    용해 일지 저장, 조회 메서드 개발 및 코드 정리

--- src/main/java/com/mirae/smartfactory/service/FurnaceProcessService.java
+++ src/main/java/com/mirae/smartfactory/service/FurnaceProcessService.java
@@ -2,9 +2,9 @@ package com.mirae.smartfactory.service;
 
 import com.mirae.smartfactory.domain.process.Process;
 import com.mirae.smartfactory.domain.process.furnace.FurnaceProcess;
-import com.mirae.smartfactory.domain.process.furnace.Ingredient;
 import com.mirae.smartfactory.dto.process.furnace.FurnaceProcessDto;
 import com.mirae.smartfactory.dto.process.furnace.FurnaceProcessListDto;
+import com.mirae.smartfactory.dto.process.furnace.FurnaceProcessQueryDto;
 import com.mirae.smartfactory.repository.FurnaceProcessRepository;
 import com.mirae.smartfactory.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
@@ -23,30 +23,33 @@ public class FurnaceProcessService {
     private final FurnaceProcessRepository furnaceProcessRepository;
     private final MemberRepository memberRepository;
 
-//    @Transactional
-//    public void saveFurnaceProcess(FurnaceProcessDto furnaceProcessDto) {
-//        Process process = Process.createProcessWithDto(furnaceProcessDto.getProcess(), memberRepository.findById(furnaceProcessDto.getProcess().getMemberId()));
-//        Ingredient ingredientAnalysis = Ingredient.createIngredientAnalysisWithDto(furnaceProcessDto.getIngredientAnalysis());
-//
-//        FurnaceProcess furnaceProcess = FurnaceProcess.createFurnaceProcessWithDto(furnaceProcessDto, process, ingredientAnalysis);
-//
-//        furnaceProcessRepository.save(furnaceProcess);
-//    }
-
     @Transactional
-    public void saveFurnaceProcess(FurnaceProcessDto furnaceProcessDto) {
+    public Long saveFurnaceProcess(FurnaceProcessDto furnaceProcessDto) {
         Process process = Process.createProcessWithDto(furnaceProcessDto.getProcess(), memberRepository.findById(furnaceProcessDto.getProcess().getMemberId()));
 
         FurnaceProcess furnaceProcess = FurnaceProcess.createFurnaceProcessWithDto(furnaceProcessDto, process);
 
         furnaceProcessRepository.save(furnaceProcess);
+
+        return furnaceProcess.getFurnaceProcessId();
+    }
+
+    @Transactional
+    public Long updateFurnaceProcess(Long furnaceProcessId, FurnaceProcessDto furnaceProcessDto) {
+        Process process = Process.createProcessWithDto(furnaceProcessDto.getProcess(), memberRepository.findById(furnaceProcessDto.getProcess().getMemberId()));
+
+        FurnaceProcess furnaceProcess = FurnaceProcess.createFurnaceProcessWithDtoAndId(furnaceProcessId, furnaceProcessDto, process);
+
+        furnaceProcessRepository.update(furnaceProcess);
+
+        return furnaceProcess.getFurnaceProcessId();
     }
 
     public FurnaceProcessListDto findFurnaceProcesses(LocalDate date) {
         List<FurnaceProcess> furnaceProcesses = furnaceProcessRepository.findListByDate(date);
 
-        List<FurnaceProcessDto> furnaceProcessDtos = furnaceProcesses.stream()
-                .map(fp -> new FurnaceProcessDto(fp))
+        List<FurnaceProcessQueryDto> furnaceProcessDtos = furnaceProcesses.stream()
+                .map(fp -> new FurnaceProcessQueryDto(fp))
                 .collect(Collectors.toList());
 
         FurnaceProcessListDto result = new FurnaceProcessListDto(furnaceProcessDtos);
