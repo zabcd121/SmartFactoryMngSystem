a7c423b841716eb94f9e99517597f01fb9271c9a
kim <zabcd121@naver.com>
2022-09-08 14:24:14 +0900

    CORS설정 및 Interceptor 설정

--- src/main/java/com/mirae/smartfactory/WebConfig.java
+++ src/main/java/com/mirae/smartfactory/WebConfig.java
@@ -3,6 +3,7 @@ package com.mirae.smartfactory;
 import com.mirae.smartfactory.interceptor.AdminLoginCheckInterceptor;
 import com.mirae.smartfactory.interceptor.LoginCheckInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,11 +15,19 @@ public class WebConfig implements WebMvcConfigurer {
 
         registry.addInterceptor(new AdminLoginCheckInterceptor())
                 .order(1)
-                .addPathPatterns("/admin/**");
+                .addPathPatterns("/statistics/**");
 
         registry.addInterceptor(new LoginCheckInterceptor())
                 .order(1)
                 .addPathPatterns("/**")
                 .excludePathPatterns("/", "/admin/**", "/login", "/logout", "/css/**", "/*.ico", "/error");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*") // 기타 설정
+                .allowedHeaders("*");;
     }
 }
