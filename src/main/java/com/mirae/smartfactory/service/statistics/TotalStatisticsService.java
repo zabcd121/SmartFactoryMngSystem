e70e6b1de5bae218ebbbae5fa7de71c8ed60798a
kim <zabcd121@naver.com>
2022-09-08 14:12:28 +0900

    4가지 도메인에 대해 일별 통계 데이터 리스트를 반환하는 메서드 개발

--- src/main/java/com/mirae/smartfactory/service/statistics/TotalStatisticsService.java
+++ src/main/java/com/mirae/smartfactory/service/statistics/TotalStatisticsService.java
@@ -1,6 +1,7 @@
 package com.mirae.smartfactory.service.statistics;
 
 import com.mirae.smartfactory.domain.statistics.Statistics;
+import com.mirae.smartfactory.domain.statistics.StatisticsType;
 import com.mirae.smartfactory.dto.statistics.DailyStatisticsListDto;
 import com.mirae.smartfactory.dto.statistics.StatisticsListDto;
 import lombok.RequiredArgsConstructor;
@@ -10,14 +11,18 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
-public class StatisticsService {
+public class TotalStatisticsService {
 
-    private final AshesStatisticsService ashesStatisticsService;
+    private final StatisticsService statisticsService;
 
     public DailyStatisticsListDto findTotalDomainDailyStatistics() {
-        Statistics ashes = ashesStatisticsService.findAshesAmountForCurrent7Days();
+        Statistics ashes = statisticsService.findDailyAvgAndDeviationDataForCurrent7Days(StatisticsType.ASHES);
+        Statistics billet = statisticsService.findDailyAvgAndDeviationDataForCurrent7Days(StatisticsType.BILLET);
+        Statistics defective = statisticsService.findDailyAvgAndDeviationDataForCurrent7Days(StatisticsType.DEFECTIVE);
+        Statistics material = statisticsService.findDailyAvgAndDeviationDataForCurrent7Days(StatisticsType.MATERIAL);
 
-        StatisticsListDto statisticsListDto = new StatisticsListDto(ashes, null, null, null);
+
+        StatisticsListDto statisticsListDto = new StatisticsListDto(ashes, billet, defective, material);
 
         return new DailyStatisticsListDto(statisticsListDto);
     }
