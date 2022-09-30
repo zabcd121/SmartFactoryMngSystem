4d76ecabe8614b3079c65e0a66c9d4ee312b3236
zabcd121 <zabcd121@naver.com>
2022-09-30 14:50:27 +0900

    fix: Jwt 토큰 관련 속성 정의

--- src/main/java/com/mirae/smartfactory/config/jwt/JwtProperties.java
+++ src/main/java/com/mirae/smartfactory/config/jwt/JwtProperties.java
@@ -1,8 +1,8 @@
 package com.mirae.smartfactory.config.jwt;
 
 public interface JwtProperties {
-    String SECRET = "ZvSJ3UcPjU";
-    int EXPIRATION_TIME = 864000000; // 10일 (1/1000초)
+    String SECRET_KEY = "k941kas1ktz46";
+    long EXPIRATION_TIME = 864000000; // 10일 (1/1000초): 864000000      30 * 60 * 1000L
     String TOKEN_PREFIX = "Bearer ";
-    String HEADER_STRING = "Authorization";
+    String HEADER_AUTHORIZATION = "Authorization";
 }
