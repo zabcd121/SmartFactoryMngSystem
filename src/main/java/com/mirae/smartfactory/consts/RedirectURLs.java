d0bc96a4f0cef23e9ec1479fc2917e201f13d286
zabcd121 <zabcd121@naver.com>
2022-09-30 14:56:42 +0900

    remove: redirectURLs class 사용하지 않아서 삭제

--- src/main/java/com/mirae/smartfactory/consts/RedirectURLs.java
+++
@@ -1,7 +0,0 @@
-package com.mirae.smartfactory.consts;
-
-public class RedirectURLs {
-
-    public static final String FURNACE_PROCESS_REDIRECT_URL = "http://localhost:8080/mirae/furnaceprocess?date=";
-    public static final String CASTING_REDIRECT_URL = "http://localhost:8080/mirae/casting?date=";
-}
