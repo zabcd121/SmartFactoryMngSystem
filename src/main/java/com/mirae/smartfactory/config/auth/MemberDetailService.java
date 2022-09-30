4b73196b63683f7a6b884a6f44b4ea500be61f0f
zabcd121 <zabcd121@naver.com>
2022-09-30 14:44:25 +0900

    refactor: loadUserByUsername() 메서드 내에서 member를 검색한 반환값을 optional로 바꿔서 예외처리 로직 실행하도록 변경

--- src/main/java/com/mirae/smartfactory/config/auth/MemberDetailService.java
+++ src/main/java/com/mirae/smartfactory/config/auth/MemberDetailService.java
@@ -1,6 +1,6 @@
 package com.mirae.smartfactory.config.auth;
 
-import com.mirae.smartfactory.domain.resource.Member;
+import com.mirae.smartfactory.domain.model.resource.Member;
 import com.mirae.smartfactory.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,7 +18,7 @@ public class MemberDetailService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String userPk) throws UsernameNotFoundException {
-        System.out.println("PrincipalDetailsService loadUserByUsername: 진입");
+        System.out.println("MemberDetailsService loadUserByUsername: 진입");
         Member member = memberRepository.findByIdInAuthorization(Long.parseLong(userPk))
                 .orElseThrow(() -> new UsernameNotFoundException(String.format("`%s`는 잘못된 아이디입니다.", userPk)));
 //        if(member == null) throw new UsernameNotFoundException(String.format("User Not Found `%s`", username));
