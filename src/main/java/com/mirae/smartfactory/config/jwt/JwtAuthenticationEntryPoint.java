f600a8710f81011730fa9c3cab5e3950bcf28d75
zabcd121 <zabcd121@naver.com>
2022-09-30 14:47:23 +0900

    fix: 토큰의 case별로 응답 객체 생성하도록 변경

--- src/main/java/com/mirae/smartfactory/config/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/mirae/smartfactory/config/jwt/JwtAuthenticationEntryPoint.java
@@ -1,6 +1,9 @@
 package com.mirae.smartfactory.config.jwt;
 
+import com.mirae.smartfactory.consts.ConditionCode;
+import com.mirae.smartfactory.consts.DomainConditionCode;
 import lombok.extern.slf4j.Slf4j;
+import org.json.JSONObject;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -9,6 +12,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+
+import static com.mirae.smartfactory.consts.ConditionCode.*;
 
 @Slf4j
 @Component
@@ -25,5 +30,38 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 //
 //        request.setAttribute("response.failure.code", unAuthorizationCode.name());
 //        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, unAuthorizationCode.message());
+        String exception = (String)request.getAttribute("exception");
+        log.error("exception: {}", exception);
+
+        if(exception.equals(null)) {
+            setResponse(response, UNKNOWN_ERROR);
+        }
+        //잘못된 타입의 토큰인 경우
+        else if(exception.equals(WRONG_TYPE_TOKEN.getCode())) {
+            setResponse(response, WRONG_TYPE_TOKEN);
+        }
+        //토큰 만료된 경우
+        else if(exception.equals(EXPIRED_TOKEN.getCode())) {
+            setResponse(response, EXPIRED_TOKEN);
+        }
+        //지원되지 않는 토큰인 경우
+        else if(exception.equals(UNSUPPORTED_TOKEN.getCode())) {
+            setResponse(response, UNSUPPORTED_TOKEN);
+        }
+        else {
+            setResponse(response, ACCESS_DENIED);
+        }
+    }
+
+    private void setResponse(HttpServletResponse response, ConditionCode exceptionCode) throws IOException {
+        response.setContentType("application/json;charset=UTF-8");
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+        JSONObject responseJson = new JSONObject();
+        responseJson.put("code", exceptionCode.getCode());
+        responseJson.put("message", exceptionCode.getMessage());
+
+
+        response.getWriter().print(responseJson);
     }
 }
