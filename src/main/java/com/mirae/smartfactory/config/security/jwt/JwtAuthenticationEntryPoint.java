89572e7d1352ad3b54e42a9119d0f8dfbe7e6c3c
zabcd121 <zabcd121@naver.com>
2022-10-13 15:38:06 +0900

    feat: 토큰과 관련하여 전달된 예외 종류에 따라 response message 생성 기능 추가

--- src/main/java/com/mirae/smartfactory/config/security/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/mirae/smartfactory/config/security/jwt/JwtAuthenticationEntryPoint.java
@@ -1,13 +1,13 @@
-package com.mirae.smartfactory.config.jwt;
+package com.mirae.smartfactory.config.security.jwt;
 
 import com.mirae.smartfactory.consts.ConditionCode;
+import io.jsonwebtoken.ExpiredJwtException;
 import lombok.extern.slf4j.Slf4j;
 import org.json.JSONObject;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -21,35 +21,27 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
     public void commence(
             HttpServletRequest request,
             HttpServletResponse response,
-            AuthenticationException e) throws IOException {
+            AuthenticationException authException) throws IOException {
 
-        log.error("Responding with unauthorized error. Message - {}", e.getMessage());
+        log.error("Responding with unauthorized error. Message - {}", authException.getMessage());
 
-//        ErrorCode unAuthorizationCode = (ErrorCode) request.getAttribute("unauthorization.code");
-//
-//        request.setAttribute("response.failure.code", unAuthorizationCode.name());
-//        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, unAuthorizationCode.message());
-        String exception = (String)request.getAttribute("exception");
-        log.error("exception: {}", exception);
+        Exception exception = (Exception) request.getAttribute("exception");
 
-        if(exception.equals(null)) {
-            setResponse(response, UNKNOWN_ERROR);
-        }
-        //잘못된 타입의 토큰인 경우
-        else if(exception.equals(WRONG_TYPE_TOKEN.getCode())) {
-            setResponse(response, WRONG_TYPE_TOKEN);
-        }
-        //토큰 만료된 경우
-        else if(exception.equals(EXPIRED_TOKEN.getCode())) {
-            setResponse(response, EXPIRED_TOKEN);
-        }
-        //지원되지 않는 토큰인 경우
-        else if(exception.equals(UNSUPPORTED_TOKEN.getCode())) {
-            setResponse(response, UNSUPPORTED_TOKEN);
-        }
-        else {
+        log.error("exception: {}", exception.getMessage());
+
+        if(exception.equals(null)){
+            log.error("null");
             setResponse(response, ACCESS_DENIED);
         }
+        else if(exception instanceof ExpiredJwtException) {
+            log.error("jwt 만료");
+            setResponse(response, EXPIRED_TOKEN);
+        }
+        else {
+            log.error("else");
+            setResponse(response, WRONG_TYPE_TOKEN);
+        }
+
     }
 
     private void setResponse(HttpServletResponse response, ConditionCode exceptionCode) throws IOException {
