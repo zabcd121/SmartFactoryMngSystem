58696d619aa1e5d5e972c7b5b87d823cb7a3131e
zabcd121 <zabcd121@naver.com>
2022-10-13 15:35:35 +0900

    feat: 권한 부족시 예외 처리 기능 추가

--- src/main/java/com/mirae/smartfactory/config/security/jwt/JwtAccessDeniedHandler.java
+++ src/main/java/com/mirae/smartfactory/config/security/jwt/JwtAccessDeniedHandler.java
@@ -1,24 +1,28 @@
 package com.mirae.smartfactory.config.security.jwt;
 
 import com.mirae.smartfactory.consts.ConditionCode;
+import lombok.extern.slf4j.Slf4j;
 import org.json.JSONObject;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+@Component
+@Slf4j
+public class JwtAccessDeniedHandler implements AccessDeniedHandler {
     @Override
-    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException{
         setResponse(response, ConditionCode.ACCESS_DENIED);
     }
 
     private void setResponse(HttpServletResponse response, ConditionCode exceptionCode) throws IOException {
+        log.info("403 handling ....");
         response.setContentType("application/json;charset=UTF-8");
-        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
 
         JSONObject responseJson = new JSONObject();
 
