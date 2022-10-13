5f9eb9bf2fdafd129f32d47cd02cf5355749eb80
zabcd121 <zabcd121@naver.com>
2022-10-13 15:07:53 +0900

    feat: redis를 이용하여 jwt 인증 방식 로그인, 토큰 재발행 기능추가

--- src/main/java/com/mirae/smartfactory/application/service/MemberService.java
+++ src/main/java/com/mirae/smartfactory/application/service/MemberService.java
@@ -1,26 +1,119 @@
-package com.mirae.smartfactory.service;
+package com.mirae.smartfactory.application.service;
 
+import com.mirae.smartfactory.config.redis.RefreshRedisRepository;
+import com.mirae.smartfactory.config.redis.RefreshRedisToken;
+import com.mirae.smartfactory.config.security.MemberDetailsService;
+import com.mirae.smartfactory.config.security.jwt.JwtTokenProvider;
 import com.mirae.smartfactory.domain.model.resource.Member;
+import com.mirae.smartfactory.dto.TokenResponseDto;
+import com.mirae.smartfactory.dto.member.LoginResDto;
+import com.mirae.smartfactory.dto.member.SimpleMemberInfo;
 import com.mirae.smartfactory.exception.InvalidPWException;
 import com.mirae.smartfactory.exception.NotExistIdException;
 import com.mirae.smartfactory.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Service
+@Slf4j
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class MemberService {
 
     private final MemberRepository memberRepository;
-    public Member login(String loginId, String password) {
-        Optional<Member> memberOptional = memberRepository.findByLoginId(loginId);
-        memberOptional.orElseThrow(() -> new NotExistIdException("존재하지 않는 아이디입니다."));
+    private final PasswordEncoder passwordEncoder;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final MemberDetailsService myUserDetailsService;
+    private final RefreshRedisRepository refreshRedisRepository;
+//    public Member login(String loginId, String password) {
+//        Optional<Member> memberOptional = memberRepository.findByLoginId(loginId);
+//        memberOptional.orElseThrow(() -> new NotExistIdException("존재하지 않는 아이디입니다."));
+//
+//        return memberOptional.filter(m -> m.getPassword().equals(password))
+//                .orElseThrow(() -> new InvalidPWException("잘못된 비밀번호입니다."));
+//    }
 
-        return memberOptional.filter(m -> m.getPassword().equals(password))
-                .orElseThrow(() -> new InvalidPWException("잘못된 비밀번호입니다."));
+    @Transactional(readOnly = true)
+    public LoginResDto signIn(String loginId, String password) {
+        // uesrId 확인
+        UserDetails userDetails = myUserDetailsService.loadUserByUsername(loginId);
+
+        // pw 확인
+        if(!passwordEncoder.matches(password, userDetails.getPassword())){
+            throw new BadCredentialsException(userDetails.getUsername() + " Invalid password");
+        }
+
+        Authentication authentication =  new UsernamePasswordAuthenticationToken(
+                userDetails.getUsername(), userDetails.getPassword(), userDetails.getAuthorities());
+
+        // refresh token 발급 및 저장
+        String refreshToken = jwtTokenProvider.createRefreshToken(authentication);
+        RefreshRedisToken token = RefreshRedisToken.createToken(loginId, refreshToken);
+
+        // 기존 토큰이 있으면 수정, 없으면 생성
+        refreshRedisRepository.save(token);
+
+        // accessToken과 refreshToken 리턴
+        TokenResponseDto tokenResponseDto = TokenResponseDto.builder()
+                .accessToken("Bearer " + jwtTokenProvider.createAccessToken(authentication))
+                .refreshToken("Bearer " + refreshToken)
+                .build();
+
+        Member member = memberRepository.findByLoginId(loginId).orElseGet(null);
+        log.info("#########member: {}", member.getMemberId());
+
+        SimpleMemberInfo simpleMemberInfo = new SimpleMemberInfo(member.getMemberId(), member.getRoleType());
+
+        return new LoginResDto(tokenResponseDto, simpleMemberInfo);
+
+    }
+
+    @Transactional(readOnly = true)
+    public TokenResponseDto reissueAccessToken(String refreshToken) {
+        log.info("refresh: {}", refreshToken);
+
+        //token 앞에 "Bearer " 제거
+        String resolveToken = resolveToken(refreshToken);
+
+        //토큰 검증 메서드
+        //실패시 jwtTokenProvider.validateToken(resolveToken) 에서 exception을 리턴함
+        jwtTokenProvider.validateToken(resolveToken);
+
+        Authentication authentication = jwtTokenProvider.getAuthentication(resolveToken);
+        // 디비에 있는게 맞는지 확인
+        RefreshRedisToken refreshRedisToken = refreshRedisRepository.findById(authentication.getName()).get();
+
+        // 토큰이 같은지 확인
+        if(!resolveToken.equals(refreshRedisToken.getToken())){
+            throw new RuntimeException("not equals refresh token");
+        }
+
+        // 재발행해서 저장
+        String newToken = jwtTokenProvider.createRefreshToken(authentication);
+        RefreshRedisToken newRedisToken = RefreshRedisToken.createToken(authentication.getName(), newToken);
+        refreshRedisRepository.save(newRedisToken);
+
+        // accessToken과 refreshToken 모두 재발행
+        return TokenResponseDto.builder()
+                .accessToken("Bearer "+jwtTokenProvider.createAccessToken(authentication))
+                .refreshToken("Bearer "+newToken)
+                .build();
+    }
+
+    //token 앞에 "Bearer-" 제거
+    private String resolveToken(String token){
+        log.info(token);
+        if(token.startsWith("Bearer "))
+            return token.substring(7);
+        throw new RuntimeException("not valid refresh token !!");
     }
 }
